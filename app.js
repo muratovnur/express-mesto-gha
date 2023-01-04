41676e118fa6aa81ff31851cc936d994c0dfd1d4
muratovnur <juzetty01@gmail.com>
2023-01-04 21:46:34 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const app = express();
+
+// При записи адреса как localhost возникала ошибка.
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63ab2a53bab19685b5864fb6',
+  };
+
+  next();
+});
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
