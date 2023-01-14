b0c56f3f3356974879ac725435c3e20bdb75ca5d
muratovnur <juzetty01@gmail.com>
2023-01-14 14:46:52 +0600

    added validator for card link field

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -10,6 +11,11 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      // без default возникает ошибка: validator.isUrl() is not a function
+      validator: (v) => validator.default.isURL(v),
+      message: 'Некорректный URL',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
